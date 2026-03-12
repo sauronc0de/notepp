4146605df8e0619d36508eaa26a380beaffbbae9
sergi.verdaguer <sergi.verdaguer@elausa.es>
2026-03-12 15:49:37 +0000

    Small change

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main(int, char **)
 {
   engine::logger.start({.queue_capacity = 8192, .color = true});
   engine::logger.set_level_mask(0xFF);
-  LOG_INFO("🗒️ Notepp started 🗒️");
+  LOG_INFO("🗒️  Notepp started 🗒️");
   App app;
   return app.run();
 }
