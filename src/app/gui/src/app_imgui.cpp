a33b33b9df340209c312f9ccad95c83d50d89925
sergi-verdaguer <sverdaguer.elausa@gmail.com>
2026-08-04 09:52:25 +0000

    Added FONT to allow to use gliph characters like: ─  │  ┌  ┐  └  ┘ ├  ┤  ┬  ┴  ┼ ━  ┃  ┏  ┓  ┗  ┛ ┣  ┫  ┳  ┻  ╋ ╭  ╮  ╰  ╯ ═  ║  ╔  ╗  ╚  ╝ ╠  ╣  ╦  ╩  ╬

--- src/app/gui/src/app_imgui.cpp
+++ src/app/gui/src/app_imgui.cpp
@@ -41,6 +41,8 @@ void App::init_imgui()
   terminal_font_config.PixelSnapH = true;
   ImFont *font_terminal = io.Fonts->AddFontDefault(&terminal_font_config);
 
+  const std::string noto_sans_mono_path = (config_.assetsPath / "fonts" / "NotoSansMono-Regular.ttf").string();
+
   auto merge_emoji_fallback = [&](const char *emoji_font_path) {
     ImFontConfig emoji_cfg;
     emoji_cfg.MergeMode = true;
@@ -58,8 +60,24 @@ void App::init_imgui()
     io.Fonts->AddFontFromFileTTF(emoji_font_path, kUiFontSize, &emoji_cfg, emoji_ranges);
   };
 
+  auto merge_box_drawing_fallback = [&](ImFont *destination_font) {
+    if(destination_font == nullptr) return;
+
+    ImFontConfig box_drawing_cfg;
+    box_drawing_cfg.MergeMode = true;
+    box_drawing_cfg.PixelSnapH = true;
+    static const ImWchar box_drawing_ranges[] = {0x2500, 0x257F, 0};
+    io.Fonts->AddFontFromFileTTF(noto_sans_mono_path.c_str(), kUiFontSize, &box_drawing_cfg,
+                                 box_drawing_ranges);
+  };
+
+  merge_box_drawing_fallback(font_terminal);
+
   const std::string roboto_path = (config_.assetsPath / "fonts" / "Roboto-Medium.ttf").string();
   const std::string opensans_path = (config_.assetsPath / "fonts" / "opensans.ttf").string();
+  const std::string italic_path = (config_.assetsPath / "fonts" / "Roboto-Italic.ttf").string();
+  const std::string bold_path = (config_.assetsPath / "fonts" / "Roboto-Bold.ttf").string();
+  const std::string twemoji_path = (config_.assetsPath / "fonts" / "twemoji.ttf").string();
   ImFont *font_regular = io.Fonts->AddFontFromFileTTF(roboto_path.c_str(), kUiFontSize);
   if(font_regular) default_font_path_ = roboto_path;
   if(!font_regular)
@@ -69,19 +87,22 @@ void App::init_imgui()
   }
   if(font_regular)
   {
-    merge_emoji_fallback((config_.assetsPath / "fonts" / "twemoji.ttf").string().c_str());
+    merge_box_drawing_fallback(font_regular);
+    merge_emoji_fallback(twemoji_path.c_str());
   }
 
-  ImFont *font_italic = io.Fonts->AddFontFromFileTTF((config_.assetsPath / "fonts" / "Roboto-Italic.ttf").string().c_str(), kUiFontSize);
+  ImFont *font_italic = io.Fonts->AddFontFromFileTTF(italic_path.c_str(), kUiFontSize);
   if(font_italic)
   {
-    merge_emoji_fallback((config_.assetsPath / "fonts" / "twemoji.ttf").string().c_str());
+    merge_box_drawing_fallback(font_italic);
+    merge_emoji_fallback(twemoji_path.c_str());
   }
 
-  ImFont *font_bold = io.Fonts->AddFontFromFileTTF((config_.assetsPath / "fonts" / "Roboto-Bold.ttf").string().c_str(), kUiFontSize);
+  ImFont *font_bold = io.Fonts->AddFontFromFileTTF(bold_path.c_str(), kUiFontSize);
   if(font_bold)
   {
-    merge_emoji_fallback((config_.assetsPath / "fonts" / "twemoji.ttf").string().c_str());
+    merge_box_drawing_fallback(font_bold);
+    merge_emoji_fallback(twemoji_path.c_str());
   }
 
   // Fallback if you don’t have files yet:
